53b3c88cf151aee8905f456d530c29818675a49c
lilmonkeylv <62300189+lilmonkeylv@users.noreply.github.com>
2025-06-12 22:25:17 +0300

    Update build.gradle.kts
    Allow 251.* build for new IntelliJ version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ val buildDataList = listOf(
         ideaSDKShortVersion = "231",
         ideaSDKVersion = "2023.1",
         sinceBuild = "231",
-        untilBuild = "243.*"
+        untilBuild = "251.*"
     ),
     BuildData(
         ideaSDKShortVersion = "223",
